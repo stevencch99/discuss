c329a5b27f2f4b3ffca68d5db60e660ac264d7a2
Steven C <stevencch99@gmail.com>
2021-06-22 00:47:56 +0800

    Create Topic model, implement :new, and :create routes/view

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -1,7 +1,16 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
 
-  def new(conn, params) do
+  alias Discuss.Topic
 
+  def new(conn, params) do
+    # struct = %Discuss.Topic{}
+    # params = ${}
+    changeset = Topic.changeset(%Topic{}, %{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, params) do
+    render(conn)
   end
 end
