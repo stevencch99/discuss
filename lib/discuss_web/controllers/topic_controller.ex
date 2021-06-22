1c31cf9d3e26d356c3d8309d68569aa003ae3a3e
Steven C <stevencch99@gmail.com>
2021-06-22 23:33:35 +0800

    Handling Failed Form Submissions

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -2,15 +2,24 @@ defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
 
   alias Discuss.Topic
+  alias Discuss.Repo
 
-  def new(conn, params) do
+  def new(conn, _params) do
     # struct = %Discuss.Topic{}
     # params = ${}
     changeset = Topic.changeset(%Topic{}, %{})
+
+    # passing @changeset to template
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, params) do
-    render(conn)
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} -> IO.inspect(post)
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+    end
   end
 end
