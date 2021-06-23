33e66dcb68df9a418a5f87dc05e8e3f5defdf8f7
Steven C <stevencch99@gmail.com>
2021-06-23 15:45:50 +0800

    Fix TopicController#update

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -40,11 +40,8 @@ defmodule DiscussWeb.TopicController do
   end
 
   def update(conn, %{"id" => topic_id, "topic" => topic}) do
-    # old_topic = Repo.get(Topic, topic_id)
-    # changeset = Topic.changeset(old_topic, topic)
-
-    # optimize
-    changeset = Repo.get(Topic, topic_id) |> Topic.changeset(topic)
+    old_topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(old_topic, topic)
 
     case Repo.update(changeset) do
       {:ok, _topic} ->
@@ -52,7 +49,7 @@ defmodule DiscussWeb.TopicController do
         |> put_flash(:info, "Topic Update")
         |> redirect(to: Routes.topic_path(conn, :index))
       {:error, changeset} ->
-        render conn, "edit.html", changeset: changeset
+        render conn, "edit.html", changeset: changeset, topic: old_topic
     end
   end
 end
