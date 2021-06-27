9ea5692c9d3da1a1aa98129d82bd8ae2cee204b8
Steven C <stevencch99@gmail.com>
2021-06-27 15:54:48 +0800

    Controller Scoped Plugs

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -3,6 +3,8 @@ defmodule DiscussWeb.TopicController do
 
   alias Discuss.{Topic, Repo}
 
+  plug Discuss.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete]
+
   def index(conn, _params) do
     topics = Repo.all(Topic)
 
